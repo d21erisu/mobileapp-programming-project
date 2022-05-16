a9cad5cf6607d4625649ed269a7e044dc773a8d0
Erik <d21erisu@student.his.se>
2022-05-16 11:54:21 +0200

    created Animal class

---
+++ app/src/main/java/com/example/project/Animal.java
@@ -0,0 +1,13 @@
+package com.example.project;
+
+public class Animal {
+
+    private String ID;
+    private String Login;
+    private String name;
+    private String race;
+    private String type;
+    private String diet;
+    private int weight;
+
+}
