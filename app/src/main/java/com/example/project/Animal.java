0b67b814d7c63c8b7bcb34e23e3f7b2da86ab82e
Erik <d21erisu@student.his.se>
2022-05-16 14:53:25 +0200

    added getters for animal class

--- app/src/main/java/com/example/project/Animal.java
+++ app/src/main/java/com/example/project/Animal.java
@@ -23,5 +23,42 @@ public class Animal {
         this.cost = cost;
         this.auxdata = auxdata;
     }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public com.example.project.auxdata getAuxdata() {
+        return auxdata;
+    }
 }
 
