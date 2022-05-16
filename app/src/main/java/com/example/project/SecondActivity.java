42ec755a9c7ed59ea215ed458e9cbb2bc8b6c062
Erik <d21erisu@student.his.se>
2022-05-16 11:46:47 +0200

    added about screen

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -3,12 +3,28 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class SecondActivity extends AppCompatActivity {
+    private WebView webView;
+
+
+
+    public void showInternalWebPage(){
+
+        webView.loadUrl("file:///android_asset/img/about.html");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        webView=findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        showInternalWebPage();
+
+
     }
 }
