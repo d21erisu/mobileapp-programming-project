38545177a07e24bb717f825d3bcfbb61a09ba9dd
Erik <d21erisu@student.his.se>
2022-05-23 10:38:16 +0200

    removed constructor from animal class

--- app/src/main/java/com/example/project/Animal.java
+++ app/src/main/java/com/example/project/Animal.java
@@ -2,28 +2,15 @@ package com.example.project;
 
 public class Animal {
 
-    private String id;
-    private String name;
-    private String type;
-    private String company;
-    private String location;
-    private String category;
-    private int size;
-    private int cost;
-    private auxdata auxdata;
-
-    public Animal(String id, String name, String type, String company, String location, String category, int size, int cost) {
-        this.id = id;
-        this.name = name;
-        this.type = type;
-        this.company = company;
-        this.location = location;
-        this.category = category;
-        this.size = size;
-        this.cost = cost;
-        this.auxdata = auxdata;
-    }
-
+    String id;
+    String name;
+    String type;
+    String company;
+    String location;
+    String category;
+    int size;
+    int cost;
+    auxdata auxdata;
 
     public String getId() {
         return id;
@@ -60,5 +47,6 @@ public class Animal {
     public com.example.project.auxdata getAuxdata() {
         return auxdata;
     }
+
 }
 
